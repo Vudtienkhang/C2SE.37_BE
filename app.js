e3b910af29b4ffe687a1818047d11909a7b1c60f
huyhoang01092004 <nguyenhuyhoangqn2004@gmail.com>
2026-03-17 20:00:32 +0700

    cấu hình giá cước

--- app.js
+++ app.js
@@ -13,11 +13,15 @@ import authRouter from './routers/auth.router.js';
 import uploadRouter from './routers/upload.router.js';
 import addressRouter from './routers/address.router.js';
 import adminRouter from './routers/admin.router.js';
+import pricingRouter from './routers/pricingConfig.router.js';
+
 
 app.use('/api/auth', authRouter);
 app.use('/api/auth/upload', uploadRouter);
 app.use('/api/addresses', addressRouter);
 app.use('/api/admin', adminRouter);
+app.use('/api/pricing-configs', pricingRouter);
+
 // Base route
 app.get('/', (req, res) => {
   res.send('API is running...');
