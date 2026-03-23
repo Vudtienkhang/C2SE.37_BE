0acbb5fee79c4853c8213e6c9fb753381cbd35c1
huyhoang01092004 <nguyenhuyhoangqn2004@gmail.com>
2026-03-23 19:49:40 +0700

    Fix Tim tai xe

--- routers/customer.router.js
+++ routers/customer.router.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getMyVehicles, addVehicle } from '../controllers/customer.controller.js';
+import { getMyVehicles, addVehicle, setDefaultVehicle } from '../controllers/customer.controller.js';
 
 const router = express.Router();
 
 router.get('/:userId/vehicles', getMyVehicles);
 router.post('/:userId/vehicles', addVehicle);
+router.put('/:userId/vehicles/:vehicleId/default', setDefaultVehicle);
 
 export default router;
